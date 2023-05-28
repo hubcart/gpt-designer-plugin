16392fcfb16e7beb78aecaa96c6c457dd237d716
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-27 21:59:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,36 @@
 import json
 import quart
 import quart_cors
-from quart import request
+from quart import request, make_response
 import openai
 from api_integration import generate_design_image
 
-app = quart_cors.cors(
-    quart.Quart(__name__),
-    allow_origin="https://chat.openai.com",
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Origin", "X-Requested-With", "Content-Type", "Accept"]
-)
+app = quart.Quart(__name__)
+app = quart_cors.CORS(app, allow_origin="https://chat.openai.com")
 
 # Initialize the GPT-3.5 model from OpenAI
 openai.api_key = ''
 
-# Add CORS middleware before route handlers
+# Add CORS headers using the before_request decorator
 @app.before_request
 async def add_cors_headers():
-    quart.response.headers["Access-Control-Allow-Origin"] = "https://plugin.hubcart.ai"
-    quart.response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    quart.response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
-
+    response = make_response()
+    response.headers["Access-Control-Allow-Origin"] = "https://plugin.hubcart.ai"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+    return response
 
 @app.post("/designs")
 async def generate_design():
-    request_data = await request.get_json(force=True)
-    user_input = request_data.get("user_input")
-
-    # Use the GPT-3.5 model to generate a response
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=user_input,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-
-    # Extract the generated design idea from the response
-    design_idea = "print on demand design of a dog"
-
-    return quart.jsonify({"design_idea": design_idea})
+    # Route handler code...
 
 @app.post("/images")
 async def generate_image():
-    request_data = await request.get_json(force=True)
-    design_idea = request_data.get("design_idea")
-
-    # Make the API call to generate the design image
-    image = generate_design_image(design_idea, width=512, height=512)
-
-    if image is not None:
-        # Return the generated image
-        return await quart.send_file(image, mimetype='image/png')
-    else:
-        return quart.Response(response='Error occurred during image generation.', status=500)
+    # Route handler code...
 
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
+    # Route handler code...
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5000)
