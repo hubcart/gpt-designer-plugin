4921777602b1060d8b1709fccc339a4389600395
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-28 00:48:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,24 @@ from api_integration import generate_design_image
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
+# Set your API key here
+API_KEY_FILE = "/api_key.txt"
+
 # Initialize the GPT-3.5 model from OpenAI
-openai.api_key = ''
+openai.api_key = ""
 
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "https://plugin.hubcart.ai"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
-    return response
+def load_api_key():
+    with open(API_KEY_FILE, "r") as file:
+        return file.read().strip()
 
-@app.route("/designs", methods=["POST"])
+@app.before_serving
+async def setup():
+    api_key = load_api_key()
+    openai.api_key = api_key
+
+@app.post("/design-plugin/generate")
 async def generate_design():
-    request_data = await request.get_json(force=True)
+    request_data = await quart.request.get_json(force=True)
     user_input = request_data.get("user_input")
 
     # Use the GPT-3.5 model to generate a response
@@ -33,32 +39,29 @@ async def generate_design():
     )
 
     # Extract the generated design idea from the response
-    design_idea = "print on demand design of a dog"
-
-    return add_cors_headers(quart.jsonify({"design_idea": design_idea}))
-
-@app.route("/images", methods=["POST"])
-async def generate_image():
-    request_data = await request.get_json(force=True)
-    design_idea = request_data.get("design_idea")
+    design_idea = "print on demand design of a dog"  # Replace with your logic to extract the design idea
 
     # Make the API call to generate the design image
     image = generate_design_image(design_idea, width=512, height=512)
 
     if image is not None:
-        # Return the generated image
-        return add_cors_headers(await quart.send_file(image, mimetype='image/png'))
-    else:
-        return add_cors_headers(quart.Response(response='Error occurred during image generation.', status=500))
+        # Convert the image to base64 for sending in the response
+        image_base64 = "<base64-encoded-image>"  # Replace with your logic to convert the image to base64
 
-@app.route("/.well-known/ai-plugin.json", methods=["GET"])
-async def plugin_manifest():
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return add_cors_headers(quart.Response(text, mimetype="text/json"))
+        # Create the response data
+        response_data = {
+            "design_idea": design_idea,
+            "image_base64": image_base64
+        }
+        return quart.Response(response=json.dumps(response_data), status=200, mimetype="application/json")
+    else:
+        error_data = {
+            "error": "Error occurred during image generation."
+        }
+        return quart.Response(response=json.dumps(error_data), status=500, mimetype="application/json")
 
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5003)
 
 if __name__ == "__main__":
     main()
