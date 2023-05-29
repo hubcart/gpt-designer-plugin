d0b8cd7ef72766b8b6cdb88d018a7316bf29576a
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-29 00:44:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,23 +38,6 @@ async def handle_create_design():
     except Exception as e:
         return quart.Response(response="Error occurred during API call: " + str(e), status=500)
 
-@app.get("/logo.png")
-async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
-
-@app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
-
-@app.get("/openapi.yaml")
-async def openapi_spec():
-    with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
-
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
 
