bd9354909779c93403f2742a517201a811ac0f88
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-28 18:37:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,42 @@
 import json
-
 import quart
 import quart_cors
 from quart import request
-import openai
-from api_integration import generate_design_image
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Set your API key here
-API_KEY_FILE = "api_key.txt"
+# Keep track of user information. Does not persist if Python session is restarted.
+USERS = {}
 
-# Initialize the GPT-3.5 model from OpenAI
-openai.api_key = ""
+@app.get("/user/")
+async def get_user():
+    # Make the API call to retrieve user information
+    # Replace this with your actual API call implementation
+    # For simplicity, this example returns a static response
+    response = "User information"
+    return quart.Response(response=response, status=200)
 
-def load_api_key():
-    with open(API_KEY_FILE, "r") as file:
-        return file.read().strip()
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
 
-@app.before_serving
-async def setup():
-    api_key = load_api_key()
-    openai.api_key = api_key
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    host = request.headers['Host']
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
 
-@app.post("/design-plugin/generate")
-async def generate_design():
-    request_data = await quart.request.get_json(force=True)
-    user_input = request_data.get("user_input")
-
-    # Use the GPT-3.5 model to generate a response
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=user_input,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-
-    # Extract the generated design idea from the response
-    design_idea = "print on demand design of a dog"  # Replace with your logic to extract the design idea
-
-    # Make the API call to generate the design image
-    image = generate_design_image(design_idea, width=512, height=512)
-
-    if image is not None:
-        # Convert the image to base64 for sending in the response
-        image_base64 = "<base64-encoded-image>"  # Replace with your logic to convert the image to base64
-
-        # Create the response data
-        response_data = {
-            "design_idea": design_idea,
-            "image_base64": image_base64
-        }
-        return quart.Response(response=json.dumps(response_data), status=200, mimetype="application/json")
-    else:
-        error_data = {
-            "error": "Error occurred during image generation."
-        }
-        return quart.Response(response=json.dumps(error_data), status=500, mimetype="application/json")
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    host = request.headers['Host']
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
 
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5003)
 
 if __name__ == "__main__":
     main()
