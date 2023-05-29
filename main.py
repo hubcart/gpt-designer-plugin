fc72ae37b80b341a311052accf102e254cb32ad3
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-28 23:36:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,34 @@
-import json
 import aiohttp
-
+import json
 import quart
-import quart_cors
-from quart import request
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
-
+# Asynchronously creates a design based on the provided prompt
 async def create_design(prompt):
-    url = "https://try.hubcart.ai:8001/sdapi/v1/txt2img"
-    headers = {"accept": "application/json", "Content-Type": "application/json"}
-    data = {"prompt": prompt}
+    url = "https://try.hubcart.ai:8001"  # API endpoint for creating designs
+    headers = {"accept": "application/json", "Content-Type": "application/json"}  # Request headers
+    data = {"prompt": prompt}  # Request payload with the prompt
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, json=data) as response:
-            if response.status == 200:
-                return await response.json()
+            if response.status == 200:  # If the API call is successful
+                return await response.json()  # Return the design information in JSON format
             else:
-                return None
+                return None  # Return None if the API call fails
 
-@app.post("/create-design")
+# HTTP POST endpoint to handle the creation of a design
+@app.post("/sdapi/v1/txt2img")
 async def handle_create_design():
     try:
-        prompt = "Design a retro/vintage Father's Day card featuring an old 1968 Pontiac GTO that is red. Include a heartfelt message like 'Happy Father's Day, Dad!'"
+        data = await request.json  # Extract the request payload
+        prompt = data.get("prompt")  # Get the prompt from the payload
 
-        design_info = await create_design(prompt)
-        if design_info:
-            return quart.Response(response=json.dumps(design_info), status=200)
+        if prompt:  # If a valid prompt is provided
+            design_info = await create_design(prompt)  # Create the design using the prompt
+            if design_info:  # If the design is created successfully
+                return quart.Response(response=json.dumps(design_info), status=200)  # Return the design information as JSON
+            else:
+                return quart.Response(response="Failed to create the design", status=500)  # Return an error message if the design creation fails
         else:
-            return quart.Response(response="Failed to create the design", status=500)
+            return quart.Response(response="Invalid request payload", status=400)  # Return an error message for an invalid request payload
     except Exception as e:
-        return quart.Response(response="Error occurred during API call: " + str(e), status=500)
-
-@app.get("/logo.png")
-async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
-
-@app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
-
-@app.get("/openapi.yaml")
-async def openapi_spec():
-    with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
-
-def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
-
-if __name__ == "__main__":
-    main()
+        return quart.Response(response="Error occurred during API call: " + str(e), status=500)  # Return an error message for any exceptions during the API call
