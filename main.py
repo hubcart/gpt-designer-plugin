0d22c26809e10a24aca8726ed1c4892a3f6083f0
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-28 20:16:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import quart
 import quart_cors
 from quart import request
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="*")
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 async def fetch_user():
     url = "https://try.hubcart.ai/user/"
