1476fe9f4095fb072078be3772cf88cc2c90b312
hubcart <131904991+hubcart@users.noreply.github.com>
2023-05-29 00:46:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import json
-import asyncio
 import aiohttp
 import quart
 import quart_cors
-from quart import request, Response
+from quart import request
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 async def create_design(prompt):
-    # Your design creation logic here
-    # Return the design information or image as a string
+    url = "https://try.hubcart.ai:8001/sdapi/v1/txt2img"
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+    data = {"prompt": prompt}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=data) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                return None
 
 @app.post("/create-design")
 async def handle_create_design():
@@ -20,23 +27,30 @@ async def handle_create_design():
         if prompt:
             design_info = await create_design(prompt)
             if design_info:
-                # Chunked Transfer-Encoding implementation
-                async def stream_response():
-                    # Split the response into smaller chunks
-                    chunk_size = 4096
-                    for i in range(0, len(design_info), chunk_size):
-                        chunk = design_info[i:i+chunk_size]
-                        yield chunk
-                        await asyncio.sleep(0.1)  # Optional delay to control chunk rate
-
-                headers = {"Content-Type": "text/plain", "Transfer-Encoding": "chunked"}
-                return Response(stream_response(), headers=headers)
+                return quart.Response(response=json.dumps(design_info), status=200)
             else:
                 return quart.Response(response="Failed to create the design", status=500)
         else:
             return quart.Response(response="Invalid request payload", status=400)
     except Exception as e:
         return quart.Response(response="Error occurred during API call: " + str(e), status=500)
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5003)
