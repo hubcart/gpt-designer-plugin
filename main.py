eec3a93477385f027c531f51971784f5e247df24
hubcart <131904991+hubcart@users.noreply.github.com>
2023-06-16 19:59:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 import json
 import aiohttp
-from quart import Quart, request
+import quart
+import quart_cors
+from quart import request
 
-app = Quart(__name__)
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 async def create_design(prompt):
-    # Read the API key from api-key.txt file
-    with open("api-key.txt", "r") as f:
-        api_key = f.read().strip()
-
-    url = "https://try.hubcart.ai/images/generations"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    }
+    url = "https://app.try.hubcart.ai/sdapi/v1/txt2img"
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
     data = {
         "prompt": prompt,
-        "n": 1,
-        "size": "256x256",
-        "response_format": "url"
+        "send_images": False,
+        "save_images": True
     }
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, json=data) as response:
             if response.status == 200:
                 response_json = await response.json()
-                image_url = response_json["data"][0]["url"]
-                return {"image_url": image_url}
+                # Extract the seed from the response
+                seed = response_json.get("info", {}).get("seed")
+                return {"seed": seed} if seed else None
             else:
                 return None
 
-@app.route("/images/generations", methods=["POST"])
-async def handle_image_generation():
+@app.post("/create-design")
+async def handle_create_design():
     try:
         data = await request.json
         prompt = data.get("prompt")
@@ -39,13 +34,33 @@ async def handle_image_generation():
         if prompt:
             design_info = await create_design(prompt)
             if design_info:
-                return json.dumps(design_info)
+                return quart.Response(response=json.dumps(design_info), status=200)
             else:
-                return "Failed to create the design", 500
+                return quart.Response(response="Failed to create the design", status=500)
         else:
-            return "Invalid request payload", 400
+            return quart.Response(response="Invalid request payload", status=400)
     except Exception as e:
-        return "Error occurred during API call: " + str(e), 500
+        return quart.Response(response="Error occurred during API call: " + str(e), status=500)
+
+@app.get("/logo.png")
+async def plugin_logo():
+    filename = 'logo.png'
+    return await quart.send_file(filename, mimetype='image/png')
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    with open("./.well-known/ai-plugin.json") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/json")
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    with open("openapi.yaml") as f:
+        text = f.read()
+        return quart.Response(text, mimetype="text/yaml")
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5003)
+    main()
